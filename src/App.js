bd52ef0b7f29c6f62f923c952335e9f6237661e5
alexneumayr <alex.nmr@outlook.com>
2025-01-28 22:08:49 +0100

    Add mobile Testimonials

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ export default function App() {
         <AboutMe />
         <Services />
         <GetInTouch />
-        {/* <Testimonials />
+        <Testimonials />
         <ReadyToGrow />
-        <Footer /> */}
+        {/* <Footer /> */}
       </main>
     </div>
   );
